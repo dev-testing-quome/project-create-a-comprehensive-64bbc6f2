c4bb9a1a093f0162b99cb341d34332b2245c0448
jim-schwoebel <jim-schwoebel@users.noreply.github.com>
2025-07-24 02:29:25 +0000

    🎨 Add frontend code and user interface

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import PatientRegistration from './pages/PatientRegistration';
+import PatientDashboard from './pages/PatientDashboard';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<PatientRegistration />} />
+        <Route path="/dashboard" element={<PatientDashboard />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
